b5edd7be8309845daea7e461f04d8c352e50311e
Geovanna Fagundes <geovanna.fagundes@hotmail.com>
2022-11-14 12:38:54 -0300

    criando componentes para o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
